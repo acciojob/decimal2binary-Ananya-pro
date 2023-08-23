00a30568803a47200f76dea21de22df9dd3988f6
Ananya Yaduvanshi <133091353+Ananya-pro@users.noreply.github.com>
2023-08-23 14:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ function decimalToBinary(num) {
 	console.log(decimalToBinary(7));
 	console.log(decimalToBinary(10))
 	console.log(decimalToBinary(33))
-}
+
 
 window.decimalToBinary = decimalToBinary;
